f1b80a4a1aa758ac3889f1ecd5da4e40e8d2423d
Brian Aniceto <anicetobrian.anthony@gmail.com>
2026-03-21 00:38:17 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/brian-portfolio/", // ✅ VERY IMPORTANT
   plugins: [react(), tailwindcss()],
 })
